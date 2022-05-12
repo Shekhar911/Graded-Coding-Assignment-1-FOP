a4e2bb5e2f3837c298f3084914fcef87bb4d723f
shekhar <shekhar.csbhatt911@gmail.com>
2022-05-12 16:52:37 +0530

    Initial commit

---
+++ src/com/company/AdminDepartment.java
@@ -0,0 +1,12 @@
+package com.company;
+public class AdminDepartment extends SuperDepartment {
+    public String deparmentName() {
+        return " Welcome to Admin Department";
+    }
+    public String getTodaysWork() {
+        return "Commplete your documents submission";
+    }
+    public String getWorkDeadline() {
+        return "Complete by EOD ";
+    }
+}
